3256ca2747600ed65f60dc0e6e31d0f743638e61
paniss <panani_97@hotmail.com>
2017-10-07 12:13:30 +0700

    Nodejs server+twig

---
+++ controllers/home.controller.js
@@ -0,0 +1,16 @@
+exports.index = function(req,res) {
+	res.render('home.twig')
+	// let content = {
+	// 	banner: [
+	// 	{
+	// 	   id: 1,
+	// 	   url: '//image/img-1.jpg'	
+	// 	},
+	// 	{
+	// 	   id: 2,
+	// 	   url: '//image/img-1.jpg'
+	// 	}
+	//   ]
+	// }
+	// res.json(content)
+}
